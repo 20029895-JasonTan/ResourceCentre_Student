e794986fb9298584efccefff05323d58a1213d2b
jeral <jeral@DESKTOP-EDPERHI>
2021-07-22 12:18:45 +0800

    testing by jerald

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -88,7 +88,7 @@ public class ResourceCentreTest {
 	public void testDoLoanCamcorder() {
 		//fail("Not yet implemented");
 		// write your code here
-		// jerald test 2
+		// jerald test 3
 	}
 	
 	@Test
